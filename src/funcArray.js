e5d1122ec3425c25462b673500e714bd2d78d797
bgpyvi <140495624+bgpyvi@users.noreply.github.com>
2024-03-10 15:40:41 +0100

    first code funcArray.js define maxOfTwoNumbers

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,6 +1,20 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(numero1,numero2){}
+export function maxOfTwoNumbers(numero1,numero2){
+    return Math.max(numero1,numero2)
+}
+export function findLongestWord(array) {
+    let longestWord = "";
+    if(array.length==0){
+        return undefined
+    }
+    array.forEach(element => {
+        if (element.length > longestWord.length) {
+        longestWord = element;
+        }
+    });
+    return longestWord;
+}
 export function findLongestWord(array){}
 export function sumArray(array){}
 export function averageNumbers(array){}
